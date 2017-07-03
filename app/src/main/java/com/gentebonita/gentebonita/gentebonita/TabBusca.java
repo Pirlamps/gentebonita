8bb8d301dee6679016830015668a3efee0f67074
Vinicius Aledo <vinicius.aledo@gmail.com>
2017-07-03 20:59:44 -0300

    Tela busca com abas

--- app/src/main/java/com/gentebonita/gentebonita/gentebonita/TabBusca.java
+++ app/src/main/java/com/gentebonita/gentebonita/gentebonita/TabBusca.java
@@ -9,11 +9,14 @@ import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
@@ -27,7 +30,9 @@ import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -37,6 +42,7 @@ import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import com.gentebonita.gentebonita.gentebonita.adapters.PerfilAdapter;
 import com.gentebonita.gentebonita.gentebonita.models.Perfil;
+import com.google.android.gms.maps.SupportMapFragment;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -64,6 +70,9 @@ public class TabBusca  extends Fragment{
     private List<Perfil> msglista3 = new ArrayList<>();
     private RecyclerView recyclerView3;
     private PerfilAdapter mAdapter3;
+
+    LinearLayout tv1,tv2,tv3,tv4,tv5;
+    ImageView i1,i1v,i2,i2v,i3,i3v,i4,i4v,i5,i5v;
 
     RequestQueue requestQueue;
     public TabBusca() {
@@ -112,23 +121,165 @@ public class TabBusca  extends Fragment{
             private Float nota;
         */
 
-        Perfil c1 = new Perfil(1,"Pessoa 1"," 25 anos","",9.52f);
+        Perfil c1 = new Perfil(1,"Marcela Cálippo"," 25 anos","fotoslide1",9.52f);
         msglista3.add(c1);
 
-        Perfil c2 = new Perfil(1,"Pessoa 2"," 25 anos","",3.77f);
+        Perfil c2 = new Perfil(1,"Raíssa Mendes"," 25 anos","fotoslide2",10f);
         msglista3.add(c2);
 
-        Perfil c3 = new Perfil(1,"Pessoa 3"," 25 anos","",6.2f);
+        Perfil c3 = new Perfil(1,"Samara Oliveira"," 25 anos","",6.2f);
         msglista3.add(c3);
 
-        Perfil c4 = new Perfil(1,"Pessoa 4"," 25 anos","",4.75f);
+        Perfil c4 = new Perfil(1,"Roberta Pereira"," 25 anos","fotoslide3",4.75f);
         msglista3.add(c4);
 
-        Perfil c5 = new Perfil(1,"Pessoa 5"," 25 anos","",8.28f);
+        Perfil c5 = new Perfil(1,"Luíza Silva"," 25 anos","fotoslide1",8.28f);
         msglista3.add(c5);
 
 
+        tv1 = (LinearLayout) getActivity().findViewById(R.id.busca1);
+        tv2 = (LinearLayout) getActivity().findViewById(R.id.busca2);
+        tv3 = (LinearLayout) getActivity().findViewById(R.id.busca3);
+        tv4 = (LinearLayout) getActivity().findViewById(R.id.busca4);
+        tv5 = (LinearLayout) getActivity().findViewById(R.id.busca5);
+
+        i1  = (ImageView) getActivity().findViewById(R.id.mi1);
+        i1v = (ImageView) getActivity().findViewById(R.id.mi1v);
+        i2  = (ImageView) getActivity().findViewById(R.id.mi2);
+        i2v = (ImageView) getActivity().findViewById(R.id.mi2v);
+        i3  = (ImageView) getActivity().findViewById(R.id.mi3);
+        i3v = (ImageView) getActivity().findViewById(R.id.mi3v);
+        i4  = (ImageView) getActivity().findViewById(R.id.mi4);
+        i4v = (ImageView) getActivity().findViewById(R.id.mi4v);
+        i5  = (ImageView) getActivity().findViewById(R.id.mi5);
+        i5v = (ImageView) getActivity().findViewById(R.id.mi5v);
+
+
+        LinearLayout quemeuvotei = (LinearLayout) view.findViewById(R.id.mbotao1);
+        quemeuvotei.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                tv1.setVisibility(View.VISIBLE);
+                tv2.setVisibility(View.GONE);
+                tv3.setVisibility(View.GONE);
+                tv4.setVisibility(View.GONE);
+                tv5.setVisibility(View.GONE);
+
+                i1.setVisibility(View.GONE);
+                i1v.setVisibility(View.VISIBLE);
+                i2.setVisibility(View.VISIBLE);
+                i2v.setVisibility(View.GONE);
+                i3.setVisibility(View.VISIBLE);
+                i3v.setVisibility(View.GONE);
+                i4.setVisibility(View.VISIBLE);
+                i4v.setVisibility(View.GONE);
+                i5.setVisibility(View.VISIBLE);
+                i5v.setVisibility(View.GONE);
+            }
+        });
+
+        LinearLayout quemvotouemmim = (LinearLayout) view.findViewById(R.id.mbotao2);
+        quemvotouemmim.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                tv1.setVisibility(View.GONE);
+                tv2.setVisibility(View.VISIBLE);
+                tv3.setVisibility(View.GONE);
+                tv4.setVisibility(View.GONE);
+                tv5.setVisibility(View.GONE);
+
+                i1.setVisibility(View.VISIBLE);
+                i1v.setVisibility(View.GONE);
+                i2.setVisibility(View.GONE);
+                i2v.setVisibility(View.VISIBLE);
+                i3.setVisibility(View.VISIBLE);
+                i3v.setVisibility(View.GONE);
+                i4.setVisibility(View.VISIBLE);
+                i4v.setVisibility(View.GONE);
+                i5.setVisibility(View.VISIBLE);
+                i5v.setVisibility(View.GONE);
+            }
+        });
+
+        LinearLayout favoritos = (LinearLayout) view.findViewById(R.id.mbotao3);
+        favoritos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                tv1.setVisibility(View.GONE);
+                tv2.setVisibility(View.GONE);
+                tv3.setVisibility(View.VISIBLE);
+                tv4.setVisibility(View.GONE);
+                tv5.setVisibility(View.GONE);
+
+                i1.setVisibility(View.VISIBLE);
+                i1v.setVisibility(View.GONE);
+                i2.setVisibility(View.VISIBLE);
+                i2v.setVisibility(View.GONE);
+                i3.setVisibility(View.GONE);
+                i3v.setVisibility(View.VISIBLE);
+                i4.setVisibility(View.VISIBLE);
+                i4v.setVisibility(View.GONE);
+                i5.setVisibility(View.VISIBLE);
+                i5v.setVisibility(View.GONE);
+            }
+        });
+
+
+        LinearLayout vips = (LinearLayout) view.findViewById(R.id.mbotao4);
+        vips.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                tv1.setVisibility(View.GONE);
+                tv2.setVisibility(View.GONE);
+                tv3.setVisibility(View.GONE);
+                tv4.setVisibility(View.VISIBLE);
+                tv5.setVisibility(View.GONE);
+
+                i1.setVisibility(View.VISIBLE);
+                i1v.setVisibility(View.GONE);
+                i2.setVisibility(View.VISIBLE);
+                i2v.setVisibility(View.GONE);
+                i3.setVisibility(View.VISIBLE);
+                i3v.setVisibility(View.GONE);
+                i4.setVisibility(View.GONE);
+                i4v.setVisibility(View.VISIBLE);
+                i5.setVisibility(View.VISIBLE);
+                i5v.setVisibility(View.GONE);
+            }
+        });
+
+
+        LinearLayout bloqueados = (LinearLayout) view.findViewById(R.id.mbotao5);
+        bloqueados.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                tv1.setVisibility(View.GONE);
+                tv2.setVisibility(View.GONE);
+                tv3.setVisibility(View.GONE);
+                tv4.setVisibility(View.GONE);
+                tv5.setVisibility(View.VISIBLE);
+
+                i1.setVisibility(View.VISIBLE);
+                i1v.setVisibility(View.GONE);
+                i2.setVisibility(View.VISIBLE);
+                i2v.setVisibility(View.GONE);
+                i3.setVisibility(View.VISIBLE);
+                i3v.setVisibility(View.GONE);
+                i4.setVisibility(View.VISIBLE);
+                i4v.setVisibility(View.GONE);
+                i5.setVisibility(View.GONE);
+                i5v.setVisibility(View.VISIBLE);
+            }
+        });
+
+
     }
+
 
 
 }
--- app/src/main/java/com/gentebonita/gentebonita/gentebonita/adapters/PerfilAdapter.java
+++ app/src/main/java/com/gentebonita/gentebonita/gentebonita/adapters/PerfilAdapter.java
@@ -28,10 +28,17 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import java.math.BigDecimal;
 
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.display.RoundedBitmapDisplayer;
+
 //import com.gentebonita.gentebonita.gentebonita.FiltroActivity;
 //import com.gentebonita.gentebonita.gentebonita.OfertaEspActivity;
 import com.gentebonita.gentebonita.gentebonita.R;
 import com.gentebonita.gentebonita.gentebonita.models.Perfil;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.display.RoundedBitmapDisplayer;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -77,6 +84,29 @@ public class PerfilAdapter extends RecyclerView.Adapter<PerfilAdapter.MyViewHold
         this.mContext = context;
     }
 
+    private Bitmap getCircleBitmap(Bitmap bitmap) {
+        final Bitmap output = Bitmap.createBitmap(bitmap.getWidth(),
+                bitmap.getHeight(), Bitmap.Config.ARGB_8888);
+        final Canvas canvas = new Canvas(output);
+
+        final int color = Color.RED;
+        final Paint paint = new Paint();
+        final Rect rect = new Rect(0, 0, bitmap.getWidth(), bitmap.getHeight());
+        final RectF rectF = new RectF(rect);
+
+        paint.setAntiAlias(true);
+        canvas.drawARGB(0, 0, 0, 0);
+        paint.setColor(color);
+        canvas.drawOval(rectF, paint);
+
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        canvas.drawBitmap(bitmap, rect, rect, paint);
+
+        bitmap.recycle();
+
+        return output;
+    }
+
 
     @Override
     public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
@@ -96,7 +126,28 @@ public class PerfilAdapter extends RecyclerView.Adapter<PerfilAdapter.MyViewHold
             holder.nome.setText(fid.getNome());
             holder.idade.setText(fid.getIdade());
             holder.nota.setText(nota.toString());
-            holder.imagem.setImageResource(R.drawable.fotoslide1);
+
+        //holder.imagem.setImageResource(R.drawable.fotoslide1);
+
+        String img1 = fid.getImagem();
+        if(img1==null||img1.equals("")||img1.equals("null"))
+        {
+            Bitmap bm = BitmapFactory.decodeResource(mContext.getResources(),
+                    R.drawable.fotodefault);
+            holder.imagem.setImageBitmap(getCircleBitmap(bm));
+        }
+        else
+        {
+            Bitmap bm = BitmapFactory.decodeResource(mContext.getResources(),
+                    R.drawable.fotoslide1);
+            holder.imagem.setImageBitmap(getCircleBitmap(bm));
+            /*ImageLoader imageLoader = ImageLoader.getInstance();
+            DisplayImageOptions options = new DisplayImageOptions.Builder().cacheInMemory(true)
+                    .displayer(new RoundedBitmapDisplayer(150))
+                    .cacheOnDisk(true).resetViewBeforeLoading(true)
+                    .build();
+            imageLoader.displayImage(img1, holder.imagem, options);*/
+        }
     }
     @Override
     public int getItemCount() {
